81c57bb9a7536ec5c37b315ede2fd900df59a93f
imRYiUK <imahmedtidiane@gmail.com>
2025-05-23 00:30:14 +0000

    auth

--- prisma/migrations/20250522210112_v1/migration.sql
+++ prisma/migrations/20250522210112_v1/migration.sql
@@ -39,17 +39,6 @@ CREATE TABLE `AuthLog` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `RevokedToken` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `token` VARCHAR(191) NOT NULL,
-    `expiresAt` DATETIME(3) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    UNIQUE INDEX `RevokedToken_token_key`(`token`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `_RoleToUser` (
     `A` INTEGER NOT NULL,
     `B` INTEGER NOT NULL,
