6ab8a67fff69e5e25588f0c971368603947acd76
imRYiUK <imahmedtidiane@gmail.com>
2025-06-29 13:18:56 +0000

    chore: update CORS configuration in main.ts to restrict origin to specific frontend URL for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   
   // Configuration CORS
   app.enableCors({
-    origin: '*',
+    origin: 'https://tele-medecine-next.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type'],
